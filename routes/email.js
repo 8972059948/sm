444037558b47c69818583d45008fbc70c397493b
8972059948 <7105871+8972059948@users.noreply.github.com>
2018-10-05 15:54:13 +0530

    Update email.js

--- routes/email.js
+++ routes/email.js
@@ -5,8 +5,8 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'kuntalkantidas.besu@gmail.com',			//email ID
-        pass: '9735084573`'	    //Password 
+        user: 'kuntalkantidas.besu@gmail.com',		//email ID
+        pass: '9735084573`'	    			//Password 
     }
 });
    
@@ -24,19 +24,19 @@ module.exports = router;
 const sendMail = (email , otp) => {
     
     var details = {
-        from: 'kuntalkantidas.besu@gmail.com', // sender address same as above
-        to: email, 					// Receiver's email id
-        subject: 'Your demo OTP is ', // Subject of the mail.
-        html: otp					// Sending OTP 
+	from: 'kuntalkantidas.besu@gmail.com', 		// sender address same as above
+	to: email, 					// Receiver's email id
+	subject: 'Your demo OTP is ', 			// Subject of the mail.
+	html: otp					// Sending OTP 
     };
 
 
 	transporter.sendMail(details, function (error, data) {
 		if(error) {
-            console.log("Response Message:",error.message)
-        }
+			console.log("Response Message:",error.message)
+		}
 		else {
-            console.log(data);
-        }
+			console.log(data);
+		}
 	});
 }
